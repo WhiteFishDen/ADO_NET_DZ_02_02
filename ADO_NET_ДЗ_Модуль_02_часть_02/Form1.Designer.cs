cd285f3878a317c0b05942a21f24bd50a97565a6
muslinazavtrak@gmail.com <muslinazavtrak@gmail.com>
2024-01-23 02:10:43 +0300

    Добавьте файлы проекта.

---
+++ ADO_NET_ДЗ_Модуль_02_часть_02/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace ADO_NET_ДЗ_Модуль_02_часть_02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dgv = new System.Windows.Forms.DataGridView();
+            this.btn_show = new System.Windows.Forms.Button();
+            this.btn_update = new System.Windows.Forms.Button();
+            this.comboBox_tables = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgv
+            // 
+            this.dgv.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.RaisedHorizontal;
+            this.dgv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgv.Location = new System.Drawing.Point(12, 78);
+            this.dgv.Name = "dgv";
+            this.dgv.Size = new System.Drawing.Size(537, 360);
+            this.dgv.TabIndex = 0;
+            // 
+            // btn_show
+            // 
+            this.btn_show.Location = new System.Drawing.Point(41, 29);
+            this.btn_show.Name = "btn_show";
+            this.btn_show.Size = new System.Drawing.Size(75, 23);
+            this.btn_show.TabIndex = 1;
+            this.btn_show.Text = "SHOW";
+            this.btn_show.UseVisualStyleBackColor = true;
+            this.btn_show.Click += new System.EventHandler(this.btn_show_Click);
+            // 
+            // btn_update
+            // 
+            this.btn_update.Location = new System.Drawing.Point(143, 29);
+            this.btn_update.Name = "btn_update";
+            this.btn_update.Size = new System.Drawing.Size(75, 23);
+            this.btn_update.TabIndex = 2;
+            this.btn_update.Text = "UPDATE";
+            this.btn_update.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btn_update.UseVisualStyleBackColor = true;
+            this.btn_update.Click += new System.EventHandler(this.btn_update_Click);
+            // 
+            // comboBox_tables
+            // 
+            this.comboBox_tables.FormattingEnabled = true;
+            this.comboBox_tables.Location = new System.Drawing.Point(288, 30);
+            this.comboBox_tables.Name = "comboBox_tables";
+            this.comboBox_tables.Size = new System.Drawing.Size(121, 21);
+            this.comboBox_tables.TabIndex = 3;
+            this.comboBox_tables.Text = "TABLES";
+            this.comboBox_tables.SelectedIndexChanged += new System.EventHandler(this.comboBox_tables_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1021, 445);
+            this.Controls.Add(this.comboBox_tables);
+            this.Controls.Add(this.btn_update);
+            this.Controls.Add(this.btn_show);
+            this.Controls.Add(this.dgv);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgv)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.Button btn_show;
+        private System.Windows.Forms.Button btn_update;
+        private System.Windows.Forms.ComboBox comboBox_tables;
+    }
+}
+
---
+++ ADO_NET_ДЗ_Модуль_02_часть_02/Form1.cs
@@ -0,0 +1,179 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ADO_NET_ДЗ_Модуль_02_часть_02
+{
+    public partial class Form1 : Form
+    {
+        private NpgsqlConnection _connection;
+        private string _connString = "";
+        private NpgsqlDataAdapter _da = null;
+        private DataTable _dt;
+        private string _tableName = "";
+        public Form1()
+        {
+            InitializeComponent();
+            _connection = new NpgsqlConnection();
+            _connString = ConfigurationManager.ConnectionStrings["MyConnString"].ConnectionString;
+            _connection.ConnectionString = _connString;
+            comboBox_tables.Items.AddRange(new string[]
+            { "stationery", "buyer_company", "sales_manager","type_of_stationery","sales","arch_stationery",
+                "arch_buyer_company", "arch_sales_manager", "arch_type_of_stationery"});
+        }
+
+        private void btn_show_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                _dt = new DataTable();
+                string SelectSql = $"select * from public.{_tableName}";
+                _da = new NpgsqlDataAdapter(SelectSql, _connection);
+                _da.Fill(_dt);
+                dgv.DataSource = _dt;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erorr!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void btn_update_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (comboBox_tables.SelectedIndex == 0) EditStationery();
+                else if (comboBox_tables.SelectedIndex == 1) EditBuyerCompany();
+                else if (comboBox_tables.SelectedIndex == 2) EditSalesManager();
+                else if (comboBox_tables.SelectedIndex == 3) EditTypeOfStationery();
+                else if (comboBox_tables.SelectedIndex == 4) EditSales();
+                _da.Update(_dt);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Erorr!", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+           
+        }
+        private void EditStationery()
+        {
+            NpgsqlCommand insert_cmd = new NpgsqlCommand("insert_stationery", _connection);
+            insert_cmd.CommandType = CommandType.StoredProcedure;
+            insert_cmd.Parameters.Add("p_name", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "name");
+            insert_cmd.Parameters.Add("p_price", NpgsqlTypes.NpgsqlDbType.Numeric, 32, "price");
+            insert_cmd.Parameters.Add("p_id_type", NpgsqlTypes.NpgsqlDbType.Integer, 32, "id_type");
+            _da.InsertCommand = insert_cmd;
+            NpgsqlCommand update_cmd = new NpgsqlCommand("update_stationery", _connection);
+            update_cmd.CommandType = CommandType.StoredProcedure;
+            update_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            update_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            update_cmd.Parameters.Add("p_name", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "name");
+            update_cmd.Parameters.Add("p_price", NpgsqlTypes.NpgsqlDbType.Numeric, 32, "price");
+            update_cmd.Parameters.Add("p_id_type", NpgsqlTypes.NpgsqlDbType.Integer, 32, "id_type");
+            _da.UpdateCommand = update_cmd;
+            NpgsqlCommand delete_cmd = new NpgsqlCommand("delete_stationery", _connection);
+            delete_cmd.CommandType = CommandType.StoredProcedure;
+            delete_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            delete_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            _da.DeleteCommand = delete_cmd;
+        }
+        private void EditBuyerCompany()
+        {
+            NpgsqlCommand insert_cmd = new NpgsqlCommand("insert_buyer_company", _connection);
+            insert_cmd.CommandType = CommandType.StoredProcedure;
+            insert_cmd.Parameters.Add("p_name", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "name");
+            insert_cmd.Parameters.Add("p_adress", NpgsqlTypes.NpgsqlDbType.Varchar, 100, "adress");
+            _da.InsertCommand = insert_cmd;
+            NpgsqlCommand update_cmd = new NpgsqlCommand("update_buyer_company", _connection);
+            update_cmd.CommandType = CommandType.StoredProcedure;
+            update_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            update_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            update_cmd.Parameters.Add("p_name", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "name");
+            update_cmd.Parameters.Add("p_adress", NpgsqlTypes.NpgsqlDbType.Varchar, 100, "adress");
+            _da.UpdateCommand = update_cmd;
+            NpgsqlCommand delete_cmd = new NpgsqlCommand("delete_buyer_company", _connection);
+            delete_cmd.CommandType = CommandType.StoredProcedure;
+            delete_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            delete_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            _da.DeleteCommand = delete_cmd;
+        }
+        private void EditSalesManager()
+        {
+            NpgsqlCommand insert_cmd = new NpgsqlCommand("insert_sales_manager", _connection);
+            insert_cmd.CommandType = CommandType.StoredProcedure;
+            insert_cmd.Parameters.Add("p_firstname", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "firstname");
+            insert_cmd.Parameters.Add("p_lastname", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "lastname");
+            insert_cmd.Parameters.Add("p_profit", NpgsqlTypes.NpgsqlDbType.Numeric, 32, "profit");
+            _da.InsertCommand = insert_cmd;
+            NpgsqlCommand update_cmd = new NpgsqlCommand("update_sales_manager", _connection);
+            update_cmd.CommandType = CommandType.StoredProcedure;
+            update_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            update_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            update_cmd.Parameters.Add("p_firstname", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "firstname");
+            update_cmd.Parameters.Add("p_lastname", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "lastname");
+            update_cmd.Parameters.Add("p_profit", NpgsqlTypes.NpgsqlDbType.Numeric, 32, "profit");
+            _da.UpdateCommand = update_cmd;
+            NpgsqlCommand delete_cmd = new NpgsqlCommand("delete_sales_manager", _connection);
+            delete_cmd.CommandType = CommandType.StoredProcedure;
+            delete_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            delete_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            _da.DeleteCommand = delete_cmd;
+        }
+        private void EditTypeOfStationery()
+        {
+            NpgsqlCommand insert_cmd = new NpgsqlCommand("insert_type_of_stationery", _connection);
+            insert_cmd.CommandType = CommandType.StoredProcedure;
+            insert_cmd.Parameters.Add("p_name", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "name");
+            _da.InsertCommand = insert_cmd;
+            NpgsqlCommand update_cmd = new NpgsqlCommand("update_type_of_stationery", _connection);
+            update_cmd.CommandType = CommandType.StoredProcedure;
+            update_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            update_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            update_cmd.Parameters.Add("p_name", NpgsqlTypes.NpgsqlDbType.Varchar, 20, "name");
+            _da.UpdateCommand = update_cmd;
+            NpgsqlCommand delete_cmd = new NpgsqlCommand("delete_type_of_stationery", _connection);
+            delete_cmd.CommandType = CommandType.StoredProcedure;
+            delete_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            delete_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            _da.DeleteCommand = delete_cmd;
+        }
+        private void EditSales()
+        {
+            NpgsqlCommand insert_cmd = new NpgsqlCommand("insert_sales", _connection);
+            insert_cmd.CommandType = CommandType.StoredProcedure;
+            insert_cmd.Parameters.Add("p_date_of_sale", NpgsqlTypes.NpgsqlDbType.Date, 20, "date_of_sale");
+            insert_cmd.Parameters.Add("p_id_stationery", NpgsqlTypes.NpgsqlDbType.Integer, 16, "id_stationery");
+            insert_cmd.Parameters.Add("p_id_sales_manager", NpgsqlTypes.NpgsqlDbType.Integer, 16, "id_sales_manager");
+            insert_cmd.Parameters.Add("p_id_buyer_company", NpgsqlTypes.NpgsqlDbType.Integer, 16, "id_buyer_company");
+            insert_cmd.Parameters.Add("p_quantity_products", NpgsqlTypes.NpgsqlDbType.Integer, 16, "quantity_products");
+            _da.InsertCommand = insert_cmd;
+            NpgsqlCommand update_cmd = new NpgsqlCommand("update_sales", _connection);
+            update_cmd.CommandType = CommandType.StoredProcedure;
+            update_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            update_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            update_cmd.Parameters.Add("p_date_of_sale", NpgsqlTypes.NpgsqlDbType.Date, 20, "date_of_sale");
+            update_cmd.Parameters.Add("p_id_stationery", NpgsqlTypes.NpgsqlDbType.Integer, 16, "id_stationery");
+            update_cmd.Parameters.Add("p_id_sales_manager", NpgsqlTypes.NpgsqlDbType.Integer, 16, "id_sales_manager");
+            update_cmd.Parameters.Add("p_id_buyer_company", NpgsqlTypes.NpgsqlDbType.Integer, 16, "id_buyer_company");
+            update_cmd.Parameters.Add("p_quantity_products", NpgsqlTypes.NpgsqlDbType.Integer, 16, "quantity_products");
+            _da.UpdateCommand = update_cmd;
+            NpgsqlCommand delete_cmd = new NpgsqlCommand("delete_sales", _connection);
+            delete_cmd.CommandType = CommandType.StoredProcedure;
+            delete_cmd.Parameters.Add("p_id", NpgsqlTypes.NpgsqlDbType.Integer, 0, "id");
+            delete_cmd.Parameters["p_id"].SourceVersion = DataRowVersion.Original;
+            _da.DeleteCommand = delete_cmd;
+        }
+        private void comboBox_tables_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            _tableName = comboBox_tables.SelectedItem as string;
+        }
+
+    }
+}
